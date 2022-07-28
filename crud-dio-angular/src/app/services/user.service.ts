d3947e235e1c0b6e6b22325b9e7d10d997a7acf1
Millene Galetti <mikagaletti1@gmail.com>
2022-07-28 17:22:09 -0300

    Feat: Adiciona funcionalidade READ com a função getAll

--- crud-dio-angular/src/app/services/user.service.ts
+++ crud-dio-angular/src/app/services/user.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IUser } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  apiUrl = 'https://sheet.best/api/sheets/a8b906fe-460a-4d8f-bad9-1bd5689d154d';
+
+  constructor(private httpClient: HttpClient) { }
+
+  // C.R.U.D - CREATE, READ, UPDATE, DELETE.
+
+  getAll(): Observable<IUser[]> {
+    return this.httpClient.get<IUser[]>(this.apiUrl);
+  }
+
 }
